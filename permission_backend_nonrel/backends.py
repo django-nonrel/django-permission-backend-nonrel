d01167ee1602cc686b20fb90b88b024846c921b2
Alex Burgel <aburgel@gmail.com>
2013-12-30 18:10:31 -0500

    Update backend method signatures to match latest django changes

--- permission_backend_nonrel/backends.py
+++ permission_backend_nonrel/backends.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 
 from models import UserPermissionList, GroupPermissionList
 
@@ -11,17 +11,18 @@ class NonrelPermissionBackend(ModelBackend):
     supports_object_permissions = False
     supports_anonymous_user = True
 
-    def get_group_permissions(self, user_obj, user_perm_obj=None):
+    def get_group_permissions(self, user_obj, obj=None, user_perm_list=None):
         """
         Returns a set of permission strings that this user has through his/her
         groups.
         """
+        if user_obj.is_anonymous() or obj is not None:
+            return set()
         if not hasattr(user_obj, '_group_perm_cache'):
             perms = set([])
-            if user_perm_obj is None:
-                user_perm_obj, created = UserPermissionList.objects.get_or_create(user=user_obj)
-
-            groups = Group.objects.filter(id__in=user_perm_obj.group_fk_list)
+            if not user_perm_list:
+                user_perm_list, _ = UserPermissionList.objects.get_or_create(user=user_obj)
+            groups = Group.objects.filter(id__in=user_perm_list.group_fk_list)
             group_perm_lists = GroupPermissionList.objects.filter(group__in=list(groups))
 
             for group_perm_list in group_perm_lists:
@@ -30,18 +31,17 @@ class NonrelPermissionBackend(ModelBackend):
             user_obj._group_perm_cache = perms
         return user_obj._group_perm_cache
 
-    def get_all_permissions(self, user_obj):
-        if user_obj.is_anonymous():
+    def get_all_permissions(self, user_obj, obj=None):
+        if user_obj.is_anonymous() or obj is not None:
             return set()
         if not hasattr(user_obj, '_perm_cache'):
             try:
-                pl = UserPermissionList.objects.get(user=user_obj)
-                user_obj._perm_cache = set(pl.permission_list)
+                user_perm_list = UserPermissionList.objects.get(user=user_obj)
+                user_obj._perm_cache = set(user_perm_list.permission_list)
 
             except UserPermissionList.DoesNotExist:
-                pl = None
+                user_perm_list = None
                 user_obj._perm_cache = set()
 
-            user_obj._perm_cache.update(self.get_group_permissions(user_obj,
-                                                                   pl))
+            user_obj._perm_cache.update(self.get_group_permissions(user_obj, user_perm_list=user_perm_list))
         return user_obj._perm_cache
